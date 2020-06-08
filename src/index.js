8617f068a1182a31411eae79ded24fc0023a9b8c
lrottach <lukas.rottach.ch@outlook.com>
2020-06-08 22:36:49 +0200

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './components/styles/Index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));

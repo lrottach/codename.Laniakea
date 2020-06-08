2261a4ddb13fb766f26930998b4cb6acae0994d3
lrottach <lukas.rottach.ch@outlook.com>
2020-06-08 21:40:29 +0200

    Build wanted app structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => (
+    <React.Fragment>
+        <div className="app">
+            <div className="header"></div>
+            <div className="body">
+                <div className="content"></div>
+                <div className="sidebar"></div>
+            </div>
+            <div className="footer"></div>
+        </div>
+    </React.Fragment>
+)
+
+export default App;

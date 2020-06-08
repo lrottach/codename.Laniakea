ebf2a09866dc8c08ae317658dcab62603b569b8c
lrottach <lukas.rottach.ch@outlook.com>
2020-06-08 22:36:32 +0200

    Build navbar and including style

---
+++ src/components/elements/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
+import '../styles/NavBar.css'
+
+const NavBar = ({onChange, onSearch}) => (
+    <div className="NavBar">
+        <div className="logo ms-font-xl">
+            <strong>Project Laniakea</strong>
+        </div>
+        <div className="searchbox">
+            <SearchBox labelText="Search">
+                onChange={(newValue) => console.log('SearchBox onChange fired: ' + newValue)}
+                onSearch={(newValue) => console.log('SearchBox onSearch fired: ' + newValue)}
+            </>
+        </div>
+    </div>
+)
+
+NavBar.propTypes = {
+    onChange: T.func,
+    onSearch: T.func,
+}
+
+NavBar.defaultProps = {
+    onChange: (newValue) => console.log('SearchBox onChange fired: ' + newValue),
+    onSearch: (newValue) => console.log('SearchBox onSearch fired: ' + newValue),
+}
+
+export default NavBar
